b7ae0f947c0cab487836992378b4efe29d12872b
Zarina Panfilova <zhiyenbayeva.z@gmail.com>
2023-06-27 07:58:25 +0600

    add project

---
+++ app.js
@@ -0,0 +1,18 @@
+function slidesPlugin() {
+    const slides = document.querySelectorAll('.slide');
+    
+    for (const slide of slides) {
+      slide.addEventListener('click', ()=> {
+      clearActiveClasses();
+    
+      slide.classList.add('active')
+      })
+    }
+    
+    function clearActiveClasses() {
+      slides.forEach((slide) => {
+      slide.classList.remove('active');
+      })
+     }}
+    
+    slidesPlugin();
